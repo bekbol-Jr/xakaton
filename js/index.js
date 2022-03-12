37af96be8cec6037fb66dfa5889f1fe63520eabf
bekboloffisial <tbkg1558@gmail.com>
2022-03-12 18:33:38 +0600

    responsive

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(".hero__slider") .slick({
+    slidesToShow: 1,
+    arrows: true,
+    dots: true,
+    // responsive: [
+    //     {
+    //         breakpoint: 1200,
+    //         settings: {
+    //             slidesToShow: 2,
+    //         }
+    //     }
+    //     ]
+})
